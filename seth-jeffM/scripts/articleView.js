ccce8bcf7410df01a2429cb0ee172a0b00d47b54
SethDonohue <donohue.seth@gmail.com>
2017-10-26 15:15:30 -0700

    fixed the final main nav, setteasers

--- seth-jeffM/scripts/articleView.js
+++ seth-jeffM/scripts/articleView.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // REVIEW: Configure an object to hold all of our functions for dynamic updates and article-related event handlers.
-console.log('start');
 let articleView = {};
 
 articleView.populateFilters = function() {
@@ -41,34 +40,27 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
 
   $('#author-filter').on('change', function() {
-    console.log('author-filter start');
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      console.log('this val', $(this).val());
-      console.log('this ', $(this));
       // DONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
       //HIDE ALL ARTICLES
       $('article').hide();
-
       //Show just for THIS author
-      $(`article[data-author='${$(this).val()}']`).show();
-
+      $(`article[data-author="${$(this).val()}"]`).show();
 
     } else {
       // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('article').fadein(750);
-      $('.template').hide();
+      $('article.template').hide();
 
     }
     $('#category-filter').val('');
   });
-  console.log('author-filter end');
 };
 
 articleView.handleCategoryFilter = function() {
-  console.log('category-filter start');
   // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
@@ -81,26 +73,25 @@ articleView.handleCategoryFilter = function() {
     } else {
       $('article').fadein(750);
       $('.template').hide();
-
     }
     $('#author-filter').val('');
   });
-  console.log('category start');
 };
 
 articleView.handleMainNav = function() {
-  console.log('before click tab', $('.tab'));
   // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
-  $('.main-nav').on('click', '.tab', function (){
+  $('.main-nav').on('click', '.tab', function (e){
+    e.preventDefault();
     $('.tab-content').hide();
-    $(this).show();
+    console.log('this.data', $(this).data('content'));
+    $('#' + $(this).data('content')).show();
   });
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  // $('.main-nav .tab:first').click();
+  $('.main-nav .tab:first').click();
 };
 
 articleView.setTeasers = function() {
@@ -110,8 +101,19 @@ articleView.setTeasers = function() {
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 
-
-
+  $('article').on('click', 'a.read-on', function(e) {
+    console.log('Article click test');
+    e.preventDefault();
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn();
+      $(this).text('');
+    }
+    // } else {
+    //   $('body').animate({ scrollTop: ($(this).parent().offset().top)}, 200);
+    //   $(this).html('Read on &rarr;');
+    //   $(this).parent().find('.artcile-body *:nth-of-type(n+2)').hide();
+    // }
+  });
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -120,7 +122,7 @@ $(document).ready(function() {
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
-  // articleView.setTeasers();
+  articleView.setTeasers();
 
 
 })
